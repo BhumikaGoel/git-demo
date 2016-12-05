569b20676497a51d360088860055c611e08a76ef
Ali Mousa <amousa11@gmail.com>
2016-12-04 20:31:20 -0800

    Added print statement

--- dice.py
+++ dice.py
@@ -15,6 +15,8 @@ Types of dice:
 
 from random import randint
 
+print("A moose was here")
+
 def make_fair_dice(sides):
     """Return a die that returns 1 to SIDES with equal chance."""
     assert type(sides) == int and sides >= 1, 'Illegal value for sides'
